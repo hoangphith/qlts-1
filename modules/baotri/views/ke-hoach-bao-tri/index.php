40be8945675b7e3e5f19edbddf34be98ab264aa8
nvven <vennguyen2009@gmail.com>
2023-08-03 14:33:35 +0700

    sua dung kh bao tri

--- modules/baotri/views/ke-hoach-bao-tri/index.php
+++ modules/baotri/views/ke-hoach-bao-tri/index.php
@@ -39,13 +39,9 @@ Yii::$app->params['showExport'] = true;
             'toolbar'=> [
                 ['content'=>
                     Html::a('<i class="fas fa fa-plus" aria-hidden="true"></i> Thêm mới', ['create'],
-<<<<<<< HEAD
                     ['role'=>'modal-remote','title'=> 'Thêm mới Ke Hoach Bao Tris','class'=>'btn btn-outline-primary']).
                     Html::a('<i class="fas fa fa-plus" aria-hidden="true"></i> Thống kê', ['thongke'],
                         ['role'=>'modal-remote','title'=> 'Thống kê bảo trì','class'=>'btn btn-outline-primary']).
-=======
-                    ['role'=>'modal-remote','title'=> 'Thêm mới Kế hoạch bảo trì','class'=>'btn btn-outline-primary']).
->>>>>>> refs/remotes/origin/main
                     Html::a('<i class="fas fa fa-sync" aria-hidden="true"></i> Tải lại', [''],
                     ['data-pjax'=>1, 'class'=>'btn btn-outline-primary', 'title'=>'Tải lại']).
                     //'{toggleData}'.
